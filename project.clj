7c667bc1ba694c7fe4ee6b01c2d4d1352e81cfb9
Mike Blume <michael.blume@climate.com>
2014-07-31 16:53:45 -0700

    add license to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject bcr-om "NOT_DEPLOYED"
   :source-paths ["src/clj" "src/cljs"]
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2268"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
